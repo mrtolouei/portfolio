4f49eed3e603e26678021763b70c2dbe0608edfa
Mr Tolouei <alitolouei.1997@gmail.com>
2026-02-13 18:06:00 +0330

    fix(NuxtConfig): Add title and meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,10 +10,24 @@ export default defineNuxtConfig({
     app: {
         head: {
             link: [
-                {
-                    rel: "stylesheet",
-                    href: "https://fonts.googleapis.com/css2?family=Cascadia+Code:wght@400;600&display=swap"
-                }
+                { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Cascadia+Code:wght@400;600&display=swap"},
+                { rel: 'icon', type: 'image/png', sizes: '48x48', href: 'https://mrtolouei.ir/favicon48x48.ico' },
+                { rel: 'apple-touch-icon', sizes: '48x48', href: 'https://mrtolouei.ir/favicon48x48.ico' }
+            ],
+            title: 'Alireza Tolouei – Senior Software Engineer',
+            meta: [
+                { name: 'description', content: 'I am a Senior Software Engineer with experience in Laravel, Vue, Nuxt, Docker, and building scalable applications.' },
+                { name: 'keywords', content: 'Alireza Tolouei, Software Engineer, Laravel, Vue, Nuxt, Backend Developer' },
+                { property: 'og:title', content: 'Alireza Tolouei – Software Engineer' },
+                { property: 'og:description', content: 'I am a Senior Software Engineer with experience in Laravel, Vue, Nuxt, Docker, and building scalable applications.' },
+                { property: 'og:url', content: 'https://mrtolouei.ir' },
+                { property: 'og:type', content: 'website' },
+                { property: 'og:image', content: 'https://mrtolouei.ir/og-image.png' },
+
+                { name: 'twitter:card', content: 'summary_large_image' },
+                { name: 'twitter:title', content: 'Alireza Tolouei – Software Engineer' },
+                { name: 'twitter:description', content: 'I am a Senior Software Engineer with experience in Laravel, Vue, Nuxt, Docker, and building scalable applications.' },
+                { name: 'twitter:image', content: 'https://mrtolouei.ir/og-image.png' }
             ]
         }
     }
