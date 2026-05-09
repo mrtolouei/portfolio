a0466b30e8005ed225c9c930e5a5582010916805
Mr Tolouei <alitolouei.1997@gmail.com>
2026-05-09 09:44:29 +0330

    fix(GithubCommand): Change repositories

--- app/utils/commands/github.ts
+++ app/utils/commands/github.ts
@@ -9,12 +9,12 @@ export const github: Command = {
             `Laravel Math Captcha
 <span class="text-gray-400">A stateless, lightweight math-based CAPTCHA package for Laravel that generates a simple arithmetic challenge as an image and verifies it via signed payloads. No database, cache, or filesystem is required.</span>
 <a href="https://github.com/mrtolouei/laravel-math-captcha" target="_blank" class="text-gray-500">-> https://github.com/mrtolouei/laravel-math-captcha</a>`,
-            `Laravel Service Health Monitor
-<span class="text-gray-400">A lightweight, extendable Laravel package for monitoring the health of internal services like Database, Redis, Queue, Cache, Filesystem, and any third-party APIs.</span>
-<a href="https://github.com/mrtolouei/laravel-health-monitor" target="_blank" class="text-gray-500">-> https://github.com/mrtolouei/laravel-health-monitor</a>`,
-            `Laravel Repository Pattern
-<span class="text-gray-400">A clean and flexible base repository package for Laravel that simplifies data access, enforces the repository pattern, and keeps your code organized and testable.</span>
-<a href="https://github.com/mrtolouei/laravel-repo-mate" target="_blank" class="text-gray-500">-> https://github.com/mrtolouei/laravel-repo-mate</a>`,
+            `Laravel Service Health
+<span class="text-gray-400">A Laravel package for monitoring the health of databases, Redis, queues, filesystems, and external HTTP services.</span>
+<a href="https://github.com/mrtolouei/laravel-service-health" target="_blank" class="text-gray-500">-> https://github.com/mrtolouei/laravel-health-monitor</a>`,
+            `Laravel Repository
+<span class="text-gray-400">A clean and flexible base repository package for Laravel that simplifies data access, enforces the Repository Pattern, and keeps your application code organized, testable, and maintainable.</span>
+<a href="https://github.com/mrtolouei/laravel-repository" target="_blank" class="text-gray-500">-> https://github.com/mrtolouei/laravel-repo-mate</a>`,
         ]
         ctx.addOutput(lines.join('\n\n'))
     }
